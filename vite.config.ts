c765d0bc6dd559641e5d425f8d1117894a03eda1
bacha77 <bacha7@gmail.com>
2026-05-12 20:47:34 -0400

    deploy: update site and source

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/finance/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
