2d7e2732ffdc193d7c6c8ad3f73eb1313618c055
bacha77 <bacha7@gmail.com>
2026-07-12 20:32:05 -0400

    deploy: update site and source

--- employee/main.tsx
+++ employee/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import EmployeeApp from '../src/EmployeeApp'
+import { LanguageProvider } from '../src/contexts/LanguageContext'
 import '../src/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <EmployeeApp />
+    <LanguageProvider>
+      <EmployeeApp />
+    </LanguageProvider>
   </React.StrictMode>,
 )
